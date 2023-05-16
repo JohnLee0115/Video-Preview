d0dd0dd8e0c9269c8bd163ff6e98609455d662ff
johnlee0115 <johnlee011500@gmail.com>
2023-05-15 22:40:27 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+var video = document.getElementById("catVideo");
+
+function over(element) {
+    video.play();
+}
+
+function out(element) {
+    video.pause();
+}
